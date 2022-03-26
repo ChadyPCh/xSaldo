79edd1e528d434a0436b09bfd154526303ac22db
ChadyPCh <88913382+ChadyPCh@users.noreply.github.com>
2022-03-26 04:23:46 +0100

    Update All.py

--- All.py
+++ All.py
@@ -3,7 +3,11 @@ import threading
 
 def GSB_Toushi():
   os.system("python3 GSB_Toushi.py")
+def Soushiro():
+  os.system("python3 Soushiro.py")
 
 t1= threading.Thread(target=GSB_Toushi)
+t2= threading.Thread(target=Soushiro)
 
 t1.start()
+t2.start()
